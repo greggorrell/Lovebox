c28b9eadef38186c6d801a8a9474bd7ca7f2e240
Marcel Stör <marcelstoer@users.noreply.github.com>
2020-12-28 16:54:22 +0100

    Integrate a few small improvements
    - use CA cert rather than server cert fingerprint
    - higher default fetch interval
    - more serial output
    - consistent formatting

--- lovebox.ino
+++ lovebox.ino
@@ -12,42 +12,59 @@ const String url = _url;
 const int lightValueThreshold = _lightValueThreshold;
 
 SSD1306Wire oled(0x3C, D2, D1);
-Servo myservo; 
+Servo myservo;
 int pos = 90;
 int increment = -1;
 int lightValue;
 String line;
 String mode;
-char idSaved = '0'; 
-bool wasRead = true;  
+char idSaved = '0';
+bool wasRead = true;
 
 void drawMessage(const String& message) {
   Serial.print("Drawing message....");
   oled.clear();
 
-  // Unterscheide zwischen Text und Bild
-  if(mode[0] == 't'){
-    oled.drawStringMaxWidth(0, 0, 128, message);    
-  } 
-  else {
-    for(int i = 0; i <= message.length(); i++){
+  // differentiat between *t*ext and image messages
+  if (mode[0] == 't') {
+    oled.drawStringMaxWidth(0, 0, 128, message);
+  } else {
+    for (int i = 0; i <= message.length(); i++) {
       int x = i % 129;
       int y = i / 129;
-    
-      if(message[i] == '1'){
+
+      if (message[i] == '1') {
         oled.setPixel(x, y);
       }
-    } 
-  }    
+    }
+  }
   oled.display();
   Serial.println("done.");
+}
+
+// Set time via NTP, as required for x.509 validation
+void setClock() {
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
 }
 
 void wifiConnect() {
   Serial.printf("Connecting to WiFi '%s'...", ssid);
   if (WiFi.status() != WL_CONNECTED) {
     WiFi.begin(ssid, password);
-  
+
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       Serial.print(".");
@@ -61,12 +78,13 @@ void getGistMessage() {
   Serial.println("Fetching message...");
   const int httpsPort = 443;
   const char* host = "gist.githubusercontent.com";
-  const char fingerprint[] = "CC AA 48 48 66 46 0E 91 53 2C 9C 7C 23 2A B1 74 4D 29 9D 33";
-  
-  WiFiClientSecure client;
-  client.setFingerprint(fingerprint);
+
+  BearSSL::WiFiClientSecure client;
+  BearSSL::X509List cert(digicertRootCert);
+  client.setTrustAnchors(&cert);
   if (!client.connect(host, httpsPort)) {
-    return; // Verbindung fehlgeschlagen
+    Serial.println("Failed to connect to GitHub");
+    return;
   }
 
   // add current millis as a cache-busting means
@@ -83,7 +101,7 @@ void getGistMessage() {
   }
   String id = client.readStringUntil('\n');
   Serial.printf("\tid: '%s', last processed id: '%c'\n", id.c_str(), idSaved);
-  if(id[0] != idSaved) { // new message
+  if (id[0] != idSaved) { // new message
     wasRead = 0;
     idSaved = id[0];
 
@@ -93,15 +111,15 @@ void getGistMessage() {
     Serial.println("\tmessage: " + line);
     drawMessage(line);
   } else {
-    Serial.println("\t-> message id wasn't updated");  
+    Serial.println("\t-> message id wasn't updated");
   }
 }
 
 void spinServo() {
-  myservo.write(pos);      
+  myservo.write(pos);
   delay(50);    // wait 50ms to turn servo
-  
-  if(pos == 75 || pos == 105) { // 75°-105° range
+
+  if (pos == 75 || pos == 105) { // 75°-105° range
     increment *= -1;
   }
   pos += increment;
@@ -121,13 +139,14 @@ void setup() {
   oled.setColor(WHITE);
   oled.setTextAlignment(TEXT_ALIGN_LEFT);
   oled.setFont(ArialMT_Plain_10);
-     
+
   oled.clear();
   oled.drawString(30, 30, "<3 LOVEBOX <3");
   oled.display();
   Serial.println("done.");
-  
+
   wifiConnect();
+  setClock();
 
   String readStateText = wasRead ? "was" : "wasn't";
   Serial.printf("Initial state: last processed id '%c' %s read.\n", idSaved, (wasRead ? "was" : "wasn't"));
@@ -136,15 +155,17 @@ void setup() {
 void loop() {
   if (WiFi.status() != WL_CONNECTED) {
     wifiConnect();
+    setClock();
   }
-  if(wasRead) {
-    getGistMessage();   
+  if (wasRead) {
+    getGistMessage();
   }
-  
-  while(!wasRead) {   
+
+  while (!wasRead) {
+    yield();
     spinServo();
     lightValue = analogRead(0);
-    if(lightValue > lightValueThreshold) {
+    if (lightValue > lightValueThreshold) {
       Serial.printf("Analog read value (LDR) %d above threshold of %d -> consider message read.\n", lightValue, lightValueThreshold);
       wasRead = 1;
     }
